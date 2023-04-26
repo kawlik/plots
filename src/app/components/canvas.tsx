c20e0e307f0d296b4c9c90b23b19f4f1eda5737f
kpawlik <kamil.pawlik@capgemini.com>
2023-04-26 15:54:32 +0200

    working plot

--- src/app/components/canvas.tsx
+++ src/app/components/canvas.tsx
@@ -2,24 +2,153 @@ import { Chart } from "chart.js/auto";
 import { onMount } from "solid-js";
 import { Network } from "../network/network";
 
-function classify(network: Network, x1: number, x2: number): number {
-	const outputs = network.feedForward([x1, x2]);
-
-	let maxIndex = 0;
-	let maxValue = 0;
-
-	for (let i = 0; i < outputs.length; i++) {
-		if (outputs[i] > maxValue) {
-			maxValue = outputs[i];
-			maxIndex = i;
-		}
-	}
-
-	return maxIndex;
-}
-
 export function Canvas(props: { network: Network }) {
 	// component logic
+	function classify(x: number, y: number): number {
+		const outputs = props.network.feedForward([x, y]);
+
+		let maxIndex = 0;
+		let maxValue = 0;
+
+		for (let i = 0; i < outputs.length; i++) {
+			if (outputs[i] > maxValue) {
+				maxValue = outputs[i];
+				maxIndex = i;
+			}
+		}
+
+		return maxIndex;
+	}
+
+	function rand(min: number, max: number): number {
+		if (min > max) [min, max] = [max, min];
+
+		return Math.random() * (max - min + 1) + min;
+	}
+
+	function draw(
+		ctx: CanvasRenderingContext2D,
+		crd: {
+			x0: number;
+			x1: number;
+			y0: number;
+			y1: number;
+		},
+		sizeX: number,
+		sizeY: number,
+		debug = false
+	): void {
+		const _x = crd.x0 / sizeX;
+		const _y = crd.y0 / sizeY;
+
+		const x = Math.floor(crd.x0);
+		const y = Math.floor(crd.y0);
+		const r = Math.ceil(crd.x1 - crd.x0);
+
+		ctx.fillStyle = classify(_x, _y) === 1 ? "red" : "blue";
+		ctx.fillRect(x, y, r, r);
+
+		if (debug) ctx.strokeRect(x, y, r, r);
+	}
+
+	function drawRecurse(
+		ctx: CanvasRenderingContext2D,
+		crd: {
+			x0: number;
+			x1: number;
+			y0: number;
+			y1: number;
+		},
+		sizeX: number,
+		sizeY: number,
+		deep: number,
+		debug = false
+	): void {
+		if (deep === 0) return draw(ctx, crd, sizeX, sizeY, debug);
+
+		if (crd.x1 - crd.x0 < 1) return draw(ctx, crd, sizeX, sizeY, debug);
+		if (crd.y1 - crd.y0 < 1) return draw(ctx, crd, sizeX, sizeY, debug);
+
+		let x = crd.x0 / sizeX;
+		let y = crd.y0 / sizeY;
+
+		let init = classify(x, y);
+		let pass = true;
+
+		for (let i = 0; i < deep ** 3; i++) {
+			x = rand(crd.x0, crd.x1) / sizeX;
+			y = rand(crd.y0, crd.y1) / sizeY;
+
+			let test = classify(x, y);
+
+			if (test === init) continue;
+
+			pass = false;
+			break;
+		}
+
+		if (pass) return draw(ctx, crd, sizeX, sizeY, debug);
+
+		const half_x = (crd.x1 - crd.x0) / 2;
+		const half_y = (crd.y1 - crd.y0) / 2;
+
+		drawRecurse(
+			ctx,
+			{
+				x0: crd.x0,
+				x1: crd.x0 + half_x,
+				y0: crd.y0,
+				y1: crd.y0 + half_y,
+			},
+			sizeX,
+			sizeY,
+			deep - 1,
+			debug
+		);
+
+		drawRecurse(
+			ctx,
+			{
+				x0: crd.x0,
+				x1: crd.x0 + half_x,
+				y0: crd.y0 + half_y,
+				y1: crd.y1,
+			},
+			sizeX,
+			sizeY,
+			deep - 1,
+			debug
+		);
+
+		drawRecurse(
+			ctx,
+			{
+				x0: crd.x0 + half_x,
+				x1: crd.x1,
+				y0: crd.y0,
+				y1: crd.y0 + half_y,
+			},
+			sizeX,
+			sizeY,
+			deep - 1,
+			debug
+		);
+
+		drawRecurse(
+			ctx,
+			{
+				x0: crd.x0 + half_x,
+				x1: crd.x1,
+				y0: crd.y0 + half_y,
+				y1: crd.y1,
+			},
+			sizeX,
+			sizeY,
+			deep - 1,
+			debug
+		);
+	}
+
 	onMount(function () {
 		const chart = new Chart(document.querySelector<HTMLCanvasElement>("#chart")!, {
 			type: "bubble",
@@ -57,44 +186,26 @@ export function Canvas(props: { network: Network }) {
 				{
 					id: "boundry",
 					beforeDraw(chart) {
-						const lenght = chart.width;
-						const offset = 1;
-
-						const imageData = chart.ctx.createImageData(lenght, lenght);
-
-						for (let x = 0; x < lenght; x += offset) {
-							for (let y = 0; y < lenght; y += offset) {
-								const _x = x / lenght;
-								const _y = y / lenght;
-
-								const index = 4 * (lenght * x + y);
-								const color = classify(props.network, _x, 1 - _y);
-
-								imageData.data[index + 0] = color === 1 ? 255 : 0;
-								imageData.data[index + 1] = 0;
-								imageData.data[index + 2] = color === 0 ? 255 : 0;
-								imageData.data[index + 3] = 99;
-							}
-						}
-
-						const texture = new ImageData(imageData.data, lenght, lenght);
-
-						chart.ctx.putImageData(texture, 0, 0);
+						drawRecurse(
+							chart.ctx,
+							{
+								x0: 0,
+								x1: chart.width,
+								y0: 0,
+								y1: chart.height,
+							},
+							chart.width,
+							chart.height,
+							Math.log2(Math.max(chart.width, chart.height))
+						);
 					},
 				},
 			],
 		});
 
-		chart.canvas.parentElement!.style.height = "256";
-		chart.canvas.parentElement!.style.width = "256";
-
 		window.addEventListener("updateLayer", () => chart.update());
 	});
 
 	// component layout
-	return (
-		<div>
-			<canvas id="chart" />
-		</div>
-	);
+	return <canvas id="chart" />;
 }
